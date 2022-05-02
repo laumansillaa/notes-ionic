127b9a3df9ba4c786bca845273395ccc7fad6a72
Lautaro Mansilla <85461971+laumansillaa@users.noreply.github.com>
2022-05-02 08:19:36 -0300

    -

--- proyect/back/index.js
+++ proyect/back/index.js
@@ -7,18 +7,18 @@ const {Notes} = require("./src/db.js").models
 db.sync({force: true}).then(async () => {
 
     const note = await Notes.create({
-        title: "Note 1, prueba de jasmine",
-        description: "Description, prueba de jasmine"
+        title: "Note 1, titulo default.",
+        description: "Description, nota de prueba.",
     })
 
     const noteUno = await Notes.create({
-    title: "Note 2, prueba de jasmine",
-    description: "Description, prueba de jasmine"
+    title: "Note 2, titulo default",
+    description: "Description, nota de prueba."
     })
 
     const noteDos = await Notes.create({
-        title: "Note 3, prueba de jasmine",
-        description: "Description, prueba de jasmine"
+        title: "Note 3, titulo default",
+        description: "Description, nota de prueba."
     })
 
 
